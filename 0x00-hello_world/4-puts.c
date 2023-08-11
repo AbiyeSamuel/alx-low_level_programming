7db11e566a9e3cb04e2a22accf1c4f7d71cb402c
Abiye <samuel.abiye01@gmail.com>
2023-08-10 19:14:28 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 /*
  * main - this is the main function
  *
  * Return: the value zero
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
