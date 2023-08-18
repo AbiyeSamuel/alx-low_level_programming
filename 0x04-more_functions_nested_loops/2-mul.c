cffcce8e7f53094b555c380008a6dfef5b792e43
Abiye <samuel.abiye01@gmail.com>
2023-08-18 07:12:41 -0700

    Task 0 to 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of 2 numbers
+ * @a: first parameter
+ * @b: secnd parameter
+ * Return: gives the answer of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
