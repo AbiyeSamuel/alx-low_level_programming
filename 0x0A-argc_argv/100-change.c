d0ce15cd9faa8e5cb8fefde2ca5457e1bc39ce08
Abiye <samuel.abiye01@gmail.com>
2023-09-01 22:11:14 -0700

    Advance task, minimal number of coins for change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to number
+ * @s: string
+ * Return: int converted
+ */
+
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			d++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+
+/**
+ * main - prints min number of coins to make as change
+ * @argc: arguments
+ * @argv: array of arguments
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = _atoi(argv[1]);
+	result = 0;
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
